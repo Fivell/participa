44173e228e321250dffcf997832a2ba29971ce7b
David Rodríguez <deivid.rodriguez@gmail.com>
2016-12-12 00:33:07 -0200

    Forward ssh keys

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,3 +2,5 @@ set :application, 'participa.unpaisencomu.cat'
 set :repo_url, 'https://github.com/EnComu/participa'
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system db/podemos}
+
+set :ssh_options, forward_agent: true
