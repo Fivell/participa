f14184628a81dee5450dd7f3a5e71b46f2c85b8d
David Rodríguez <deivid.rodriguez@gmail.com>
2016-12-12 00:27:43 -0200

    Let capistrano fly free

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,3 @@
-# config valid only for Capistrano 3.4.0
-lock '3.4.0'
-
 set :application, 'participa.barcelonaencomu.cat'
 set :repo_url, 'https://github.com/GuanyemBarcelona/participa'
 set :linked_files, %w{config/database.yml config/secrets.yml}
