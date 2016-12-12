23d55c37510ee9de83ea31edab4124fa72622862
David Rodríguez <deivid.rodriguez@gmail.com>
2016-12-12 10:48:24 -0200

    Only master allowed into production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 server 'participa.unpaisencomu.cat', port: 22015, roles: %w(db web app)
 
-set :branch, ENV['BRANCH'] || :master
+set :branch, :master
 set :deploy_to, '/home/participa/participa.unpaisencomu.cat'
