2cd8f7239486a192f972bc8688ea3884a71c23ed
David Rodríguez <deivid.rodriguez@gmail.com>
2016-12-15 09:15:28 -0200

    Remove `secure_headers` gem
    Those are being set at the NGINX level.

--- config/initializers/secure_headers.rb
+++
@@ -1,9 +0,0 @@
-::SecureHeaders::Configuration.default do |config|
-  config.hsts = "max-age=#{20.years.to_i}"
-  config.x_frame_options = 'SAMEORIGIN'
-  config.x_content_type_options = "nosniff"
-  config.x_xss_protection = '1; mode=block'
-  config.x_download_options = 'noopen'
-  config.x_permitted_cross_domain_policies = 'none'
-  config.csp = SecureHeaders::OPT_OUT
-end
