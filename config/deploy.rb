a945fbc1916bc6ae42e37fa0465bc49ea6e9f82a
David Rodríguez <deivid.rodriguez@gmail.com>
2016-12-18 19:40:21 -0200

    Symlink fonts on deployment
    We can't share them publicly because they are private :(

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, 'participa'
 set :repo_url, 'https://github.com/EnComu/participa'
 set :linked_files, %w{config/database.yml config/secrets.yml config/redis.yml}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system db/podemos}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system db/podemos app/assets/fonts}
 
 set :user, 'participa'
 set :ssh_options, forward_agent: true
