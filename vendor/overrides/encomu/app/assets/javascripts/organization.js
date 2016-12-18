10969fa57ee4cc4ab297f49bbd560804fa011cfd
David Rodríguez <deivid.rodriguez@gmail.com>
2016-12-18 14:49:08 -0200

    Switch header to the correct height

--- vendor/overrides/encomu/app/assets/javascripts/organization.js
+++ vendor/overrides/encomu/app/assets/javascripts/organization.js
@@ -6,7 +6,6 @@ $(function(){
     $('.js-header-others-show').on('click', function(e) {
       e.preventDefault();
       $('.js-header-others-hidden').removeClass('hide').show();
-      $('.header-content').animate({height: '170px'});
       $('.menu ul').animate({padding: '5px 0px 0px'});
     });
   }
