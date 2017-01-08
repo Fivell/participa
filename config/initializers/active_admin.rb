498d2a5655eb396cde55dcb534efcd43eb93c6e7
David Rodríguez <deivid.rodriguez@gmail.com>
2017-01-08 10:01:59 -0200

    Fix routing error in admin
    That file does not exist.

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -169,7 +169,6 @@ ActiveAdmin.setup do |config|
   #
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
-  config.register_stylesheet 'admin/podemos.css'
   config.register_javascript 'admin/podemos.js'
 
 
