a5abb64bab2abc15ed6689ff55b23cd73d791681
Andrés Pereira <apereira@alabs.org>
2017-01-11 10:09:37 +0100

    Removed unused JS on active_admin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -169,7 +169,6 @@ ActiveAdmin.setup do |config|
   #
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
-  config.register_javascript 'admin/podemos.js'
 
   # == Menu System
   #
