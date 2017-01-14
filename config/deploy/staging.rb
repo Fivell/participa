6e0c51ff33d6a06dbe8e8a660a0206ba2eac89b3
David Rodríguez <deivid.rodriguez@gmail.com>
2017-01-14 09:26:37 -0200

    Default to deploying current branch to staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 server 'betaparticipa.unpaisencomu.cat', port: 22014, roles: %w(db web app)
 
-set :branch, ENV['BRANCH'] || :master
+set :branch, -> { `git rev-parse --abbrev-ref HEAD`.chomp }
 set :deploy_to, '/home/participa/betaparticipa.unpaisencomu.cat'
