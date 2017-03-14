ce0ae45dc38d8966b25b5cc24346232e44427ccd
David Rodríguez <deivid.rodriguez@gmail.com>
2017-03-13 22:33:32 -0300

    Remove dead method

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -63,16 +63,3 @@ end
 
 Capybara.javascript_driver = :poltergeist
 Capybara.asset_host = 'http://localhost:3000'
-
-def with_versioning
-  was_enabled = PaperTrail.enabled?
-  was_enabled_for_controller = PaperTrail.enabled_for_controller?
-  PaperTrail.enabled = true
-  PaperTrail.enabled_for_controller = true
-  begin
-    yield
-  ensure
-    PaperTrail.enabled = was_enabled
-    PaperTrail.enabled_for_controller = was_enabled_for_controller
-  end
-end
