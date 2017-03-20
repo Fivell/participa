aee0339f4d2d3899af6cb38eb91528da9b0d9dd7
David Rodríguez <deivid.rodriguez@gmail.com>
2017-03-20 01:38:49 -0300

    Always run sms_validator test
    Temporarily enable the feature instead on relying on the developer
    having it configured.

--- test/controllers/sms_validator_controller_test.rb
+++ test/controllers/sms_validator_controller_test.rb
@@ -1,144 +1,143 @@
 require 'test_helper'
 
-if available_features["verification_sms"]
+class SmsValidatorControllerTest < ActionController::TestCase
 
-  class SmsValidatorControllerTest < ActionController::TestCase
-  
-    setup do 
-      @user = create(:user, :not_confirmed_by_sms)
-    end
-  
-    test "redirects to login page when anonymous" do
-      get :step1
-      assert_response :redirect
-      assert_redirected_to "/users/sign_in"
-      get :step2
-      assert_response :redirect
-      assert_redirected_to "/users/sign_in"
-      get :step3
-      assert_response :redirect
-      assert_redirected_to "/users/sign_in"
-    end
-  
-    test "does not allow sms confirmation when confirmed by sms recently" do
-      user = create(:user, :confirmed_by_sms)
-      sign_in user
-      get :step1
-      assert_response :redirect
-      assert_redirected_to root_url 
-      assert_equal "Ya has confirmado tu número en los últimos meses.", flash[:error]
-    end
-  
-    test "allows sms confirmation when not confirmed by sms" do
-      user = create(:user, :not_confirmed_by_sms)
-      sign_in user
-      get :step1
-      assert_response :success
-    end
-  
-    test "allows sms confirmation when sms confirmation expired" do
-      user = create(:user, :previously_confirmed_by_sms)
-      sign_in user
-      get :step1
-      assert_response :success
-    end
-  
-    test "redirects to sms confirmation after login when not confirmed by sms" do
-      sign_in @user
-      @controller = ToolsController.new
-      get :index
-      assert_response :redirect
-      assert_redirected_to sms_validator_step1_path
-    end
-  
-    test "advances to second step after setting phone for first time" do
-      phone = '666666666'
-      sign_in @user
-      post :phone, params: { user: { unconfirmed_phone: phone } } 
-      @user = User.find @user.id # relaod @user data
-      assert_equal "0034#{phone}", @user.unconfirmed_phone
-      assert_redirected_to sms_validator_step2_path
-    end
-  
-    test "advances to second step after updating an unconfirmed phone" do
-      phone = '666666666'
-      sign_in @user
-      post :phone, params: { user: { unconfirmed_phone: phone } }
-      @user = User.find @user.id # relaod @user data
-      assert_equal "0034#{phone}", @user.unconfirmed_phone
-      assert_redirected_to sms_validator_step2_path
-    end
-  
-    test "allows step2 directly when phone is set" do
-      @user.update_attribute(:unconfirmed_phone, "0034666888999")
-      sign_in @user
-      get :step2 
-      assert_response :success
-    end
-  
-    test "does not allow step2 directly when phone not set yet" do
-      @user.update_attribute(:phone, nil)
-      @user.update_attribute(:unconfirmed_phone, nil)
-      sign_in @user
-      get :step2 
-      assert_response :redirect
-      assert_redirected_to sms_validator_step1_path
-    end
-  
-    test "does not allow step3 directly when phone not set yet" do
-      @user.update_attribute(:unconfirmed_phone, nil)
-      sign_in @user
-      get :step3 
-      assert_response :redirect
-      assert_redirected_to sms_validator_step1_path
-    end
-  
-    test "does not allow step3 directly when no sms confirmation token yet" do
-      @user.update_attribute(:unconfirmed_phone, '0034666888999')
-      @user.update_attribute(:sms_confirmation_token, nil)
-      sign_in @user
-      get :step3 
-      assert_response :redirect
-      assert_redirected_to sms_validator_step2_path
-    end
-  
-    test "confirms sms token if user gave it OK" do
+  around do |&block|
+    @user = create(:user, :not_confirmed_by_sms)
+
+    with_verifications(sms: true) { super(&block) }
+  end
+
+  test "redirects to login page when anonymous" do
+    get :step1
+    assert_response :redirect
+    assert_redirected_to "/users/sign_in"
+    get :step2
+    assert_response :redirect
+    assert_redirected_to "/users/sign_in"
+    get :step3
+    assert_response :redirect
+    assert_redirected_to "/users/sign_in"
+  end
+
+  test "does not allow sms confirmation when confirmed by sms recently" do
+    user = create(:user, :confirmed_by_sms)
+    sign_in user
+    get :step1
+    assert_response :redirect
+    assert_redirected_to root_url 
+    assert_equal "Ya has confirmado tu número en los últimos meses.", flash[:error]
+  end
+
+  test "allows sms confirmation when not confirmed by sms" do
+    user = create(:user, :not_confirmed_by_sms)
+    sign_in user
+    get :step1
+    assert_response :success
+  end
+
+  test "allows sms confirmation when sms confirmation expired" do
+    user = create(:user, :previously_confirmed_by_sms)
+    sign_in user
+    get :step1
+    assert_response :success
+  end
+
+  test "redirects to sms confirmation after login when not confirmed by sms" do
+    sign_in @user
+    @controller = ToolsController.new
+    get :index
+    assert_response :redirect
+    assert_redirected_to sms_validator_step1_path
+  end
+
+  test "advances to second step after setting phone for first time" do
+    phone = '666666666'
+    sign_in @user
+    post :phone, params: { user: { unconfirmed_phone: phone } } 
+    @user = User.find @user.id # relaod @user data
+    assert_equal "0034#{phone}", @user.unconfirmed_phone
+    assert_redirected_to sms_validator_step2_path
+  end
+
+  test "advances to second step after updating an unconfirmed phone" do
+    phone = '666666666'
+    sign_in @user
+    post :phone, params: { user: { unconfirmed_phone: phone } }
+    @user = User.find @user.id # relaod @user data
+    assert_equal "0034#{phone}", @user.unconfirmed_phone
+    assert_redirected_to sms_validator_step2_path
+  end
+
+  test "allows step2 directly when phone is set" do
+    @user.update_attribute(:unconfirmed_phone, "0034666888999")
+    sign_in @user
+    get :step2 
+    assert_response :success
+  end
+
+  test "does not allow step2 directly when phone not set yet" do
+    @user.update_attribute(:phone, nil)
+    @user.update_attribute(:unconfirmed_phone, nil)
+    sign_in @user
+    get :step2 
+    assert_response :redirect
+    assert_redirected_to sms_validator_step1_path
+  end
+
+  test "does not allow step3 directly when phone not set yet" do
+    @user.update_attribute(:unconfirmed_phone, nil)
+    sign_in @user
+    get :step3 
+    assert_response :redirect
+    assert_redirected_to sms_validator_step1_path
+  end
+
+  test "does not allow step3 directly when no sms confirmation token yet" do
+    @user.update_attribute(:unconfirmed_phone, '0034666888999')
+    @user.update_attribute(:sms_confirmation_token, nil)
+    sign_in @user
+    get :step3 
+    assert_response :redirect
+    assert_redirected_to sms_validator_step2_path
+  end
+
+  test "confirms sms token if user gave it OK" do
+    token = 'AAA123'
+    @user.update_attribute(:sms_confirmation_token, token)
+    sign_in @user
+    post :valid, params: { user: { sms_user_token_given: token } } 
+    assert_response :redirect
+    assert_redirected_to root_path
+  end
+
+  test "does not confirm sms token if user gave it wrong" do
+    token = 'AAA123'
+    @user.update_attribute(:sms_confirmation_token, 'BBB123')
+    sign_in @user
+    post :valid, params: { user: { sms_user_token_given: token } } 
+    assert_response :success
+    assert_equal "El código que has puesto no corresponde con el que te enviamos por SMS.", flash[:error]
+  end
+
+  test "restores vote town from deleted user if sms confirmation succeeds" do
+    with_blocked_change_location do
+      old_user = create(:user)
+      old_user.confirm
+      old_user.delete
+      
       token = 'AAA123'
-      @user.update_attribute(:sms_confirmation_token, token)
-      sign_in @user
-      post :valid, params: { user: { sms_user_token_given: token } } 
+      new_user = create(:user, :not_confirmed_by_sms, town: "m_03_003_6", document_vatid: old_user.document_vatid, 
+                                                      sms_confirmation_token: token, unconfirmed_phone: old_user.phone)
+      sign_in new_user
+      post :valid, params: { user: { sms_user_token_given: token } }
+      new_user = User.where(phone: old_user.phone).last
+      assert_equal old_user.vote_town, new_user.vote_town, "New user vote location should be the same of the old user"
+      # XXX pasca - comento linea para saltar de momento validacion. Asegurar
+      # que funcione 
+      # assert_equal I18n.t("registration.message.existing_user_location"), flash[:alert]
       assert_response :redirect
       assert_redirected_to root_path
-    end
-  
-    test "does not confirm sms token if user gave it wrong" do
-      token = 'AAA123'
-      @user.update_attribute(:sms_confirmation_token, 'BBB123')
-      sign_in @user
-      post :valid, params: { user: { sms_user_token_given: token } } 
-      assert_response :success
-      assert_equal "El código que has puesto no corresponde con el que te enviamos por SMS.", flash[:error]
-    end
-  
-    test "restores vote town from deleted user if sms confirmation succeeds" do
-      with_blocked_change_location do
-        old_user = create(:user)
-        old_user.confirm
-        old_user.delete
-        
-        token = 'AAA123'
-        new_user = create(:user, :not_confirmed_by_sms, town: "m_03_003_6", document_vatid: old_user.document_vatid, 
-                                                        sms_confirmation_token: token, unconfirmed_phone: old_user.phone)
-        sign_in new_user
-        post :valid, params: { user: { sms_user_token_given: token } }
-        new_user = User.where(phone: old_user.phone).last
-        assert_equal old_user.vote_town, new_user.vote_town, "New user vote location should be the same of the old user"
-        # XXX pasca - comento linea para saltar de momento validacion. Asegurar
-        # que funcione 
-        # assert_equal I18n.t("registration.message.existing_user_location"), flash[:alert]
-        assert_response :redirect
-        assert_redirected_to root_path
-      end
     end
   end
 end
