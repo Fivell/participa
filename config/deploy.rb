d7057c7144c29a8a320b980f0717e97a481435f7
Andrés Pereira <apereira@alabs.org>
2017-03-22 12:56:08 +0100

    Staging deploy with config/mailserver.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :application, 'participa'
 set :repo_url, 'https://github.com/EnComu/participa'
-set :linked_files, %w{config/database.yml config/secrets.yml config/redis.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/redis.yml config/mailserver.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system db/podemos app/assets/fonts}
 
 set :user, 'participa'
