ab69aa93e6e457291712cda8d4f92749207558eb
David Rodríguez <deivid.rodriguez@gmail.com>
2017-03-23 14:39:48 -0300

    Use login helpers in home page tests
    They are supposed to be slower but much more reliable. Definitely want
    them in login related tests.

--- test/integration/home_test.rb
+++ test/integration/home_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
+require 'integration/concerns/login_helpers'
 
 class HomeTest < ActionDispatch::IntegrationTest
+  include Participa::Test::LoginHelpers
 
   test "shows login page for anonymous users" do
     visit root_path
@@ -9,7 +11,7 @@ class HomeTest < ActionDispatch::IntegrationTest
 
   test "redirects to edit profile when no verifications enabled" do
     with_verifications(presential: false, sms: false) do
-      login_as create(:user)
+      login create(:user)
       visit root_path
       assert_title "Datos personales"
     end
@@ -17,7 +19,7 @@ class HomeTest < ActionDispatch::IntegrationTest
 
   test "shows verification page when only presential verifications enabled" do
     with_verifications(presential: true, sms: false) do
-      login_as create(:user)
+      login create(:user)
       visit root_path
       assert_title "No te has verificado"
     end
@@ -25,7 +27,7 @@ class HomeTest < ActionDispatch::IntegrationTest
 
   test "redirects to sms verification when only sms verifications enabled" do
     with_verifications(presential: false, sms: true) do
-      login_as create(:user)
+      login create(:user)
       visit root_path
       assert_title "Introduce tu número de teléfono móvil"
     end
@@ -33,7 +35,7 @@ class HomeTest < ActionDispatch::IntegrationTest
 
   test "shows election page when only sms verifications enabled & user verified" do
     with_verifications(presential: false, sms: true) do
-      login_as create(:user, :confirmed_by_sms)
+      login create(:user, :confirmed_by_sms)
       visit root_path
       assert_title "Usuario/a verificado/a | Votación"
     end
@@ -41,7 +43,7 @@ class HomeTest < ActionDispatch::IntegrationTest
 
   test "redirects to sms verification when both verifications enabled & user not verified" do
     with_verifications(presential: true, sms: true) do
-      login_as create(:user)
+      login create(:user)
       visit root_path
       assert_title "Introduce tu número de teléfono móvil"
     end
@@ -49,7 +51,7 @@ class HomeTest < ActionDispatch::IntegrationTest
 
   test "shows election page when both verifications enabled & user verified presentially" do
     with_verifications(presential: true, sms: true) do
-      login_as create(:user, :verified_presentially)
+      login create(:user, :verified_presentially)
       visit root_path
       assert_title "Usuario/a verificado/a | Votación"
     end
@@ -57,7 +59,7 @@ class HomeTest < ActionDispatch::IntegrationTest
 
   test "shows election page when both verifications enabled & user verified by sms" do
     with_verifications(presential: true, sms: true) do
-      login_as create(:user, :confirmed_by_sms)
+      login create(:user, :confirmed_by_sms)
       visit root_path
       assert_title "Usuario/a verificado/a | Votación"
     end
