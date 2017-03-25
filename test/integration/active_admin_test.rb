eb92d265e6ab76a1913551a636a011ece8dcc9eb
David Rodríguez <deivid.rodriguez@gmail.com>
2017-03-25 18:20:54 -0300

    Improve admin access tests
    * Make them independent of enabled features.
    * Test all feature combinations.

--- test/integration/active_admin_test.rb
+++ test/integration/active_admin_test.rb
@@ -1,22 +1,63 @@
 require "test_helper"
 
 class ActiveAdminTest < ActionDispatch::IntegrationTest
-
-  test "unverified normal user shouldn't access the admin" do
+  test "anonymous users can't access the admin" do
     visit admin_users_path
     assert_content "No tienes permisos para acceder a esa sección "
-
-    user = create(:user)
-    login_as(user)
-    visit admin_collaborations_path
-    assert_content "Por seguridad, debes confirmar tu teléfono"
   end
 
-  test "verified normal user shouldn't access the admin" do
-    user = create(:user, :confirmed_by_sms)
-    login_as(user)
-    visit admin_collaborations_path
-    assert_content "No tienes permisos para acceder a esa sección "
+  test "unverified normal user shouldn't access the admin with both verifications" do
+    with_verifications(sms: true, presential: true) do
+      user = create(:user)
+      login_as(user)
+      visit admin_collaborations_path
+      assert_content "Por seguridad, debes confirmar tu teléfono"
+    end
+  end
+
+  test "unverified normal user shouldn't access the admin with only presential verifications" do
+    with_verifications(sms: false, presential: true) do
+      user = create(:user)
+      login_as(user)
+      visit admin_collaborations_path
+      assert_content "No tienes permisos para acceder a esa sección "
+    end
+  end
+
+  test "unverified normal user shouldn't access the admin with only online verifications" do
+    with_verifications(sms: true, presential: false) do
+      user = create(:user)
+      login_as(user)
+      visit admin_collaborations_path
+      assert_content "Por seguridad, debes confirmar tu teléfono"
+    end
+  end
+
+  test "verified normal user shouldn't access the admin with both verifications" do
+    with_verifications(sms: true, presential: true) do
+      user = create(:user, :confirmed_by_sms)
+      login_as(user)
+      visit admin_collaborations_path
+      assert_content "No tienes permisos para acceder a esa sección "
+    end
+  end
+
+  test "verified normal user shouldn't access the admin with only presential verifications" do
+    with_verifications(sms: false, presential: true) do
+      user = create(:user, :confirmed_by_sms)
+      login_as(user)
+      visit admin_collaborations_path
+      assert_content "No tienes permisos para acceder a esa sección "
+    end
+  end
+
+  test "verified normal user shouldn't access the admin with only online verifications" do
+    with_verifications(sms: true, presential: false) do
+      user = create(:user, :confirmed_by_sms)
+      login_as(user)
+      visit admin_collaborations_path
+      assert_content "No tienes permisos para acceder a esa sección "
+    end
   end
 
   test "admin user should access the admin" do
