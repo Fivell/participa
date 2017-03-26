b7e043b0deba1cec2db3bf853fc339fd48430aba
David Rodríguez <deivid.rodriguez@gmail.com>
2017-03-26 13:01:51 -0300

    Improve minitest custom matcher a bit
    Not sure yet why the diff is all red, but this is better anyways.

--- test/support/assertions.rb
+++ test/support/assertions.rb
@@ -6,20 +6,19 @@ module Minitest
     #  Fails unless +expected+ and +actual+ have the same items.
     #
     def assert_matches_array(expected, actual)
-      assert same_items(expected, actual), same_items_message(expected, actual)
+      assert actual.is_a?(Enumerable), invalid_type_message(actual)
+      assert expected.is_a?(Enumerable), invalid_type_message(expected)
+
+      assert_equal expected.sort, actual.sort
     end
 
     private
 
-    def same_items_message(expected, actual)
-      "Expected #{expected.inspect} and #{actual.inspect} " \
-      "to have the same items"
-    end
-
-    def same_items(expected, actual)
-      actual.is_a?(Enumerable) &&
-        expected.is_a?(Enumerable) &&
-        expected.sort == actual.sort
+    def invalid_type_message(object)
+      <<~EOM.squish
+        Expected #{object} of class #{object.class} to be an Enumerable, but
+        it's not
+      EOM
     end
   end
 end
