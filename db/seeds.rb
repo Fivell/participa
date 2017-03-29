9e049dad8d8acf32fd9afee340bb38f259a191cd
David Rodríguez <deivid.rodriguez@gmail.com>
2017-03-29 19:01:20 -0300

    Seed with verificators and people to be verified

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,23 @@ admin = FactoryGirl.create(:user,
                            email: "admin@example.com") 
 puts "Creating admin user with email #{admin.email}"
 
+presential_verifier = FactoryGirl.create(:user, :verifying_presentially)
+puts "Creating presential verifier with email #{presential_verifier.email}"
+
 superadmin = FactoryGirl.create(:user, :superadmin, password: pw, email: "superadmin@example.com") 
 puts "Creating superadmin user with email #{superadmin.email}"
 
-(0..10).each do |i| 
+(0..5).each do |i|
   user = FactoryGirl.create(:user, password: pw, email: "unverified#{i}@example.org")
   puts "Creating unverified user with email #{user.email}"
 
   user = FactoryGirl.create(:user, password: pw, email: "verified#{i}@example.org")
   user.verify! admin
   puts "Creating verified user with email #{user.email}"
+
+  user = FactoryGirl.create(:user,
+                            :confirmed_by_sms,
+                            password: pw,
+                            email: "confirmed_by_sms#{i}@example.org")
+  puts "Creating user confirmed_by_sms with email #{user.email}"
 end
