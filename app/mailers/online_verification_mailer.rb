e3e47b0b6264e439525381d0521e752c6b089ee0
David Rodríguez <deivid.rodriguez@gmail.com>
2017-03-30 10:05:21 -0300

    Fix online verification mailer language

--- app/mailers/online_verification_mailer.rb
+++ app/mailers/online_verification_mailer.rb
@@ -5,7 +5,15 @@ class OnlineVerificationMailer < ActionMailer::Base
   def report event
     @event = event
 
-    mail to: event.verified.email,
+    #
+    # @todo. Store user's locale in DB and use that locale for sending emails.
+    # For now, we use the default locale since we can't be sure and we
+    # definitely don't want the user to receive emails in the verificator's
+    # locale.
+    #
+    I18n.with_locale(I18n.default_locale) do
+      mail to: event.verified.email,
          subject: t('online_verifications.mailer.subject')
+    end
   end
 end
