ad9c2ac9b394e0aac0e09752a836b078914a050a
David Rodríguez <deivid.rodriguez@gmail.com>
2017-03-30 10:06:09 -0300

    Make sure verification mail is sent in Catalan
    We have no information on user's preferred language, so default to
    catalan for now.

--- app/mailers/verification_mailer.rb
+++ app/mailers/verification_mailer.rb
@@ -5,7 +5,16 @@ class VerificationMailer < ActionMailer::Base
   # user is verified
   def verified user
     @user = user
-    mail(to: @user.email, subject: t('verification.mailer.verified.subject'))
+
+    #
+    # @todo. Store user's locale in DB and use that locale for sending emails.
+    # For now, we use the default locale since we can't be sure and we
+    # definitely don't want the user to receive emails in the verificator's
+    # locale.
+    #
+    I18n.with_locale(I18n.default_locale) do
+      mail(to: @user.email, subject: t('verification.mailer.verified.subject'))
+    end
   end
 
   # user needs to verify
