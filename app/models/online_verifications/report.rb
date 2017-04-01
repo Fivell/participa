bb551d235e17708d7faeaddb595fc163d8b1e85e
David Rodríguez <deivid.rodriguez@gmail.com>
2017-04-01 03:19:34 -0300

    Add `dependent: destroy` to association
    This allows to easily destroy records and keep data consistent.

--- app/models/online_verifications/report.rb
+++ app/models/online_verifications/report.rb
@@ -2,7 +2,7 @@ module OnlineVerifications
   class Report < Event
     belongs_to :verifier, class_name: 'User'
 
-    has_many :issues
+    has_many :issues, dependent: :destroy
     has_many :labels, through: :issues
   end
 end
