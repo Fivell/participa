673577a6920792605588a398b86b7c07a9ddc8f7
David Rodríguez <deivid.rodriguez@gmail.com>
2017-04-01 22:32:59 -0300

    Remove an spurious number in a test file

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -70,7 +70,7 @@ class ApplicationHelperTest < ActionView::TestCase
     result = errors_in_form user
     assert_equal "", result
   end
-1
+
   test "should steps_nav work" do 
     result = steps_nav(1, %w(primero segundo tercero))
     expected = "<nav class=\"steps3\">\n  <ul>\n    <li class=active>\n      <span class=\"block\">\n        <span class=\"tab-number\">1</span>\n        <span class=\"tab-text\">[&quot;primero&quot;, &quot;segundo&quot;, &quot;tercero&quot;]</span>\n      </span>\n    </li>\n    <li >\n      <span class=\"block\">\n        <span class=\"tab-number\">2</span>\n        <span class=\"tab-text\"></span>\n      </span>\n    </li>\n    <li >\n      <span class=\"block\">\n        <span class=\"tab-number\">3</span>\n        <span class=\"tab-text\"></span>\n      </span>\n    </li>\n  </ul>\n</nav>\n\n<h3>\n  <span>[&quot;primero&quot;, &quot;segundo&quot;, &quot;tercero&quot;]</span>\n</h3>\n"
